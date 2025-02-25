9746c171a35613e8c9f783a521eb824e5df72fac
Jimmy Ramani <jimmy.softrefine@gmail.com>
2025-02-25 14:11:07 +0530

    Toolip with ViewChild decorator instead of render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   selectedPosition: 'top' | 'bottom' | 'left' | 'right' = 'top';
   selectedBgColor: string = '#002329';
   selectedFontColor: string = '#87e8de';
   selectedFontSize: string = '20px';
+
+  @ViewChild('tooltipElement') tooltipElement!: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    this.updateTooltipStyles();
+  }
+
+  updateTooltipStyles() {
+    if (this.tooltipElement) {
+      this.renderer.setStyle(
+        this.tooltipElement.nativeElement,
+        'backgroundColor',
+        this.selectedBgColor
+      );
+      this.renderer.setStyle(
+        this.tooltipElement.nativeElement,
+        'color',
+        this.selectedFontColor
+      );
+      this.renderer.setStyle(
+        this.tooltipElement.nativeElement,
+        'fontSize',
+        this.selectedFontSize
+      );
+    }
+  }
 }
